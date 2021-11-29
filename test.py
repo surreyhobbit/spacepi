7066c40b569f7ef89bd143bafabec161e5e71359
Surreyhobbit <surreyhobbit@gmail.com>
2021-11-29 20:03:56 +0100

    revert to commit 3d2022bc0d2e0f271eb18a4c8bacd41008de491b as with large files nothing was working anymore

--- test.py
+++ test.py
@@ -13,21 +13,15 @@ laser = Sound("/home/pi/space/wavs/w_laser1.wav")
 power = Sound("/home/pi/space/wavs/w_power.wav") 
 rpg = Sound("/home/pi/space/wavs/w_rpg.wav")
 missile = Sound("/home/pi/space/wavs/w_missile.wav")
-#melting = Sound("/home/pi/space/wavs/w1_self_destruct.ogg")
-nohome = Sound("/home/pi/space/wavs/w1_system_shut_down.ogg")
-# new sounds 2021 - w1 prefix
-#weapons = Sound("/home/pi/space/wavs/w1_sci_fi_weapons.ogg")
-#binary = Sound("/home/pi/space/wavs/w1_binary_code.ogg")
-shields = Sound("/home/pi/space/wavs/w1_shield_up_red_alert.wav")
+melting = Sound("/home/pi/space/wavs/ImMeltingMelting.ogg")
+nohome = Sound("/home/pi/space/wavs/NoPlaceLikeHome.ogg")
 
-# holdTime = 6 - this worked with the ImMelting sound, the self destruct sequence is longer, might have to be cut
-# holdTime = 73 - this keeps the proportion, but is too long as it is also used to set the time for "when_held"
 holdTime = 6
 offGPIO = 3
 
 def shutdown():
     # play some shutdown sound
-    #melting.stop()
+    melting.stop()
     nohome.play()
     # give it a chance to play for one second
     time.sleep(1)
@@ -37,19 +31,18 @@ def shutdown():
 
 def off_pressed():
     # start playing
-    # melting.play(loops=holdTime/melting.get_length())
-    #melting.play(loops=3)
+    melting.play(loops=holdTime/melting.get_length())
 
 def off_released():
     # stop playing if released early
-    #melting.stop()
+    melting.stop()
 
 def press_laser():
     # start playing
     laser.play()
 
 def press_power():
-    # start playing - used as slower throttle sound
+    # start playing
     power.play()
 
 def press_rpg():
@@ -58,31 +51,17 @@ def press_rpg():
 def press_missile():
     missile.play()
 
-# new functions 2021
-def press_weapons():
-    #weapons.play()
-
-def press_binary():
-    #binary.play()
-
-def press_shields():
-    shields.play()
 
 btn1 = Button(11)
 btn2 = Button(2)
 btn3 = Button(23)
 btn4 = Button(24)
-# New button: Big red fire button
-#btn5 = Button(00)
 btnOff = Button(offGPIO, hold_time=holdTime)
 
-#btn1 = when_pressed = press_binary
-#btn1.when_pressed = press_laser
-btn2.when_pressed = press_shields
-#btn2.when_pressed = press_power
+btn1.when_pressed = press_laser
+btn2.when_pressed = press_power
 btn3.when_pressed = press_rpg
 btn4.when_pressed = press_missile
-#btn5.when_pressed = press_rpg
 btnOff.when_pressed = off_pressed
 btnOff.when_released = off_released
 btnOff.when_held = shutdown
